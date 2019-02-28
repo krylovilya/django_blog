76e5e0915bde2e4448b24e03df5268ecd21fd3eb
krylovilya <krylovilya1@gmail.com>
2019-02-28 23:15:27 +0800

    create urls\views

---
+++ main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view()),
+    path('profile/<int:id>', views.ProfileView.as_view()),
+    path('auth', views.AuthView.as_view()),
+    path('register', views.RegisterView.as_view()),
+    path('add_post', views.AddPostView.as_view()),
+    path('edit_post/<int:id>', views.EditPostView.as_view()),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+
+class ProfileView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+
+class AuthView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+
+class RegisterView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+
+class AddPostView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+
+class EditPostView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        pass
