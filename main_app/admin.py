f5bd251fc01197d62562a6b21a315644b27a4b17
krylovilya <krylovilya1@gmail.com>
2019-02-28 14:14:39 +0800

    create models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class UserAdmmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.User._meta.fields]
+    search_fields = [field.name for field in models.User._meta.fields]
+
+    class Meta:
+        model = models.User
+
+
+class PostAdmmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.Post._meta.fields]
+    search_fields = [field.name for field in models.Post._meta.fields]
+
+    class Meta:
+        model = models.Post
+
+
+admin.site.register(models.User, UserAdmmin)
+admin.site.register(models.Post, PostAdmmin)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True, verbose_name="Логин")
+    password = models.CharField(max_length=150, blank=True, verbose_name="Пароль")
+    last_login = models.DateTimeField(blank=True, verbose_name="Последний вход", editable=False)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Заголовок")
+    content = models.TextField(max_length=10000, verbose_name="Текст")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return "{} (Автор: {})".format(self.title, self.author)
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
